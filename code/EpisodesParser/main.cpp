cfce77cf391588ba16eb2e23a8ec1ecb39928d0c
Wim Leers <work@wimleers.com>
2010-11-20 18:29:06 +0100

    Added timer to see how long parsing takes.

--- code/EpisodesParser/main.cpp
+++ code/EpisodesParser/main.cpp
@@ -3,9 +3,17 @@
 using namespace EpisodesParser;
 
 int main(int argc, char *argv[]) {
+    QTextStream cout(stdout);
+    QTextStream cerr(stderr);
     Parser * parser;
+    QTime timer;
+
+
     parser = new Parser();
+
+    timer.start();
     parser->parse("/Users/wimleers/School/masterthesis/logs/driverpacks.net.episodes.log");
+    cout << QString("Duration: %1 ms.").arg(timer.elapsed()) << endl;
 }
 
 
