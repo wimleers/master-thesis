d482aff20a1b8779d58d04934b923c61f3d4f075
Wim Leers <work@wimleers.com>
2010-11-21 13:05:20 +0100

    Added lines/ms and ms/line performance stats.

--- code/EpisodesParser/main.cpp
+++ code/EpisodesParser/main.cpp
@@ -16,7 +16,13 @@ int main(int argc, char *argv[]) {
 
     timer.start();
     int linesParsed = parser->parse("/Users/wimleers/School/masterthesis/logs/driverpacks.net.episodes.log");
-    cout << QString("Duration: %1 ms. Parsed %2 lines.").arg(timer.elapsed()).arg(linesParsed) << endl;
+    int timePassed = timer.elapsed();
+    cout << QString("Duration: %1 ms. Parsed %2 lines. That's %3 lines/ms or %4 ms/line.")
+            .arg(timePassed)
+            .arg(linesParsed)
+            .arg((float)linesParsed/timePassed)
+            .arg((float)timePassed/linesParsed)
+            << endl;
 }
 
 
